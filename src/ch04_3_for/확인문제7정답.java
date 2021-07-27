32f43e8dc468d5ca387aa021c214df46b36ee83a
hhm12 <hhm12@LAPTOP-08SROAN1>
2021-07-27 23:53:38 +0900

    tt

--- src/ch04_3_for/확인문제7정답.java
+++ src/ch04_3_for/확인문제7정답.java
@@ -2,10 +2,10 @@ package ch04_3_for;
 
 import java.util.Scanner;
 
-public class 확인문제7 {
+public class 확인문제7정답 {
 	public static void main(String[] args) {
 		boolean run = true;
-		int balance = 0; // 균형...돈이야??
+		int balance = 0;
 		Scanner scanner = new Scanner(System.in);
 
 		while (run) {
@@ -14,22 +14,28 @@ public class 확인문제7 {
 			System.out.println("--------------------------------");
 			System.out.print("선택> ");
 
-			int inputData = Integer.parseInt(scanner.nextLine());
+			int menuNum = Integer.parseInt(scanner.nextLine());// 문자열을 숫자로 바꾸는거
 
-			if (inputData == 1) {
+			switch (menuNum) {
+			case 1:
 				System.out.print("예금액> ");
-//				balance = Integer.parseInt(scanner.nextLine());
-				balance = balance + Integer.parseInt(scanner.nextLine());
-			} else if (inputData == 2) {
+				balance += Integer.parseInt(scanner.nextLine());
+				break;
+			case 2:
 				System.out.print("출금액> ");
-				balance = balance - Integer.parseInt(scanner.nextLine());
-			} else if (inputData == 3) {
-				System.out.println("잔고> ");
+				balance -= Integer.parseInt(scanner.nextLine());
+				break;
+			case 3:
+				System.out.print("잔고> ");
 				System.out.println(balance);
-			} else if (inputData == 4) {
+				break;
+			case 4:
+				run = false;
 				break;
 			}
+			System.out.println();
 		}
 		System.out.println("프로그램 종료");
 	}
+
 }
