a45a5ce2762ad8ce60c76626b68e05fe4d8791fd
hhm12 <hhm12@LAPTOP-08SROAN1>
2021-07-28 10:43:16 +0900

    test

--- src/ch04_3_for/구구단과제.java
+++ src/ch04_3_for/구구단과제.java
@@ -2,7 +2,7 @@ package ch04_3_for;
 
 public class 구구단과제 {
 	public static void main(String[] args) {
-		for (int i = 1; i < 10; i++) {
+		for (int i = 2; i < 10; i++) {
 			System.out.println("[" + i + "단]");
 			for (int j = 1; j < 10; j++) {
 				System.out.printf("%d X %d = %d\n", i, j, i * j);
@@ -11,3 +11,6 @@ public class 구구단과제 {
 		}
 	}
 }
+//for(int i = 0; i < 100; i++) { 이게 for의 기본 폼이니깐 왠만하면 바꾸지말고 사용하자!!!!(int i=0;부터 시작!!)
+//	
+//}
---
+++ src/traning/Test.java
@@ -0,0 +1,18 @@
+package traning;
+
+public class Test {
+	public static void main(String[] args) {
+//		이거를 if한번만 써서 출력을하자
+//		int num = 120;
+//		if (num > 0) {
+//			if ((num % 2) == 0) {
+//				System.out.println("양수이면서 짝수");
+//			}
+//		}
+
+		int num = 120;
+		if (num > 0 && (num % 2) == 0) {
+			System.out.println("양수이면서 짝수");
+		}
+	}
+}
